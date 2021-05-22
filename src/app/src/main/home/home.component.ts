3526ce9d3813ef622b0d91a5cb66a3466bc8cc17
2ujin <suddenlydisappear200@gmail.com>
2021-05-22 19:44:21 +0900

    feat: 예약페이지 마무리

--- src/app/src/main/home/home.component.ts
+++ src/app/src/main/home/home.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  public reservation_list = [
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  go_make_reservation() {
+    this.router.navigateByUrl("/make-reservation");
   }
 
+  go_reservation() {
+    this.router.navigateByUrl("/reservation");
+  }
 }
--- src/app/src/main/reservation/reservation.component.ts
+++ src/app/src/main/reservation/reservation.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-reservation',
-  templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.scss']
+  selector: "app-reservation",
+  templateUrl: "./reservation.component.html",
+  styleUrls: ["./reservation.component.scss"],
 })
 export class ReservationComponent implements OnInit {
+  public reservation_list = [
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+    {
+      text: "",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
