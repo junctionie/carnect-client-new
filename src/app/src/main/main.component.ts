2ebb0a8f22a7971e8e8113a79d225c4668631f96
2ujin <suddenlydisappear200@gmail.com>
2021-05-22 17:32:35 +0900

    fix: 헤더 로고 정리

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  go_my() {
+    this.router.navigateByUrl("/my");
   }
 
+  go_home() {
+    this.router.navigateByUrl("/home");
+  }
 }
